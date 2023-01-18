565658df175ab185b74978b95a591fbde7fb2c90
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-18 23:47:25 +0530

    [Akshay] added program for enqueue element in queue

--- Stack and Queue/Program.cs
+++ Stack and Queue/Program.cs
@@ -4,16 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Stack stack=new Stack();
-            stack.push(56);
-            stack.push(30);
-            stack.push(70);
-            stack.Display();
+            //Stack stack=new Stack();
+            //stack.push(56);
+            //stack.push(30);
+            //stack.push(70);
+            //stack.Display();
 
-            stack.peek();
-            stack.Display();
-            stack.pop();
-            stack.Display();
+            //stack.peek();
+            //stack.Display();
+            //stack.pop();
+            //stack.Display();
+
+            Queue queue = new Queue();
+            queue.enqueue(56);
+            queue.enqueue(30);
+            queue.enqueue(70);
+            queue.Display();
            
         }
     }
---
+++ Stack and Queue/Queue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_Queue
+{
+    internal class Queue
+    {
+        Node Top;
+        public void enqueue(int data)
+        {
+            Node node = new Node(data);
+            if(Top == null)
+            {
+                Top = node;
+            }
+            else
+            {
+                Node temp= Top;
+                while(temp.Next != null)
+                {
+                    temp= temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine(node.data + " is enqueued in queue");
+        }
+        public void Display()
+        {
+            Node temp= Top;
+            Console.WriteLine("\n*****Queue*****");
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.Next;
+            }
+        }
+       
+    }
+}
